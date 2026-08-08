6c4f7075e0f489b8e402ed0a494d4e3db55274ca
Pulkit Verma <pulkit.verma@foxnetglobal.com>
2026-08-08 21:28:36 +0530

    Product share card also serves as the Twitter card

---
+++ app/product/[slug]/twitter-image.js
@@ -0,0 +1,3 @@
+// Same object card on both networks. Without this file Twitter falls back to
+// the site-wide picture and the object being shared never appears.
+export { default, alt, size, contentType } from './opengraph-image';
